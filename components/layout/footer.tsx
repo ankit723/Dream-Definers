8020ab548ceb4bf6d111600925342fc50a76ffcc
dreamdefinerstrainingacademy <dreamdefinerstrainingacademy@gmail.com>
2026-02-26 14:00:43 +0530

    Update footer.tsx

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -79,7 +79,7 @@ export const Footer = () => {
             </h4>
             <ul className="flex flex-col gap-3">
              <li>
-  <Link href="/Terms-condition" className="hover:underline">
+  <Link href="/Terms-condition" className="text-white/90 hover:text-white transition-colors text-sm">
     Terms & Conditions
   </Link>
 </li>
